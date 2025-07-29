f5ac76e0bc3148587a8b215dbf313eb7b9f18da7
Marlon Beijer <marlon@eevul.org>
2025-07-29 13:09:13 +0200

    Add CPM + revert to adding specific subdirectories instead of doing it dynamically

---
+++ package-lock.cmake
@@ -0,0 +1,13 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# CPMLicenses.cmake
+CPMDeclarePackage(CPMLicenses.cmake
+  VERSION 0.0.4
+  GITHUB_REPOSITORY TheLartians/CPMLicenses.cmake
+)
+# Common CMake additions for Amiga
+CPMDeclarePackage(CMakeAmigaCommon
+  GIT_TAG 1.0.1
+  GITHUB_REPOSITORY AmigaPorts/cmake-amiga-common-library
+)
